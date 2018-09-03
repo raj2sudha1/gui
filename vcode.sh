bf5431580fa375dc2001e46c39d3c8cea45c958b
Sudhakar Y <sudhakary@hotmail.com>
2018-09-03 16:20:41 +0530

    adding files

---
+++ vcode.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# allow X11 access -- DISPLAY
+export DISPLAY=172.23.244.94:0.0
+
+# start vscode
+docker run -d --rm \
+  -v ${pwd}:/developer/project \
+  -e DISPLAY=$DISPLAY \
+  -p 5000:5000 \
+  --name vscode \
+  cmiles74/docker-vscode
+
+
+
