71ee63302be078dc93078e5031109d0e1cd9fa42
Sudhakar Y <sudhakary@hotmail.com>
2018-09-03 19:40:24 +0530

    add changes to open firefox and vscode on start

---
+++ devapps-home.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# https://github.com/cmiles74/docker-vscode
+
+# allow X11 access -- DISPLAY
+#export DISPLAY=172.23.244.94:0.0
+
+docker run -d --rm -v c:/:/home/sy -e DISPLAY=$DISPLAY -p 5000:5000 --name devapps sudhakary/devapps:v1
+docker exec -d devapps /developer/bin/gnome-www-browser
+docker exec devapps /developer/bin/start-vscode
+
---
+++ devapps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# https://github.com/cmiles74/docker-vscode
+
+# allow X11 access -- DISPLAY
+export DISPLAY=172.23.244.94:0.0
+
+docker run -d --rm -v c:/:/home/sy -e DISPLAY=$DISPLAY -p 5000:5000 --name devapps sudhakary/devapps:v1
