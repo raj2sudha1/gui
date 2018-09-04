56fa394bb442fb6538db21c49f93685130591773
Sudhakar Y <sudhakary@hotmail.com>
2018-09-04 09:47:39 +0530

    updated devapps.sh file to use the latest devapps image

--- devapps.sh
+++ devapps.sh
@@ -4,7 +4,8 @@
 # allow X11 access -- DISPLAY
 export DISPLAY=172.23.244.94:0.0
 
-docker run -d --rm -v c:/:/home/sy -e DISPLAY=$DISPLAY -p 5000:5000 --name devapps sudhakary/devapps:v1
+docker run -d --rm -v c:/:/home/sy -e DISPLAY=$DISPLAY -p 5000:5000 --name devapps sudhakary/devapps:v3
 docker exec -d devapps /developer/bin/gnome-www-browser
 docker exec devapps /developer/bin/start-vscode
+docker exec devapps /developer/bin/start-bcompare
 
